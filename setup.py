28f1968a61cab33f423e77286f1c2d4ffb1b603d
mirkobrombin <send@mirko.pm>
2021-11-11 16:37:35 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='EasyTerm',
+    description='An easy-to-bundle GTK terminal emulator',
+    author='Mirko brombin',
+    author_email='send@mirko.pm',
+    version='0.1',
+    scripts = (['easyterm'])
+)
