7f927074d405b3684a9419956955711ab81271eb
mirkobrombin <send@mirko.pm>
2021-12-11 08:31:47 +0100

    support custom palette on call

--- easyterm/easyterm.py
+++ easyterm/easyterm.py
@@ -16,15 +16,21 @@ CONF_FG = Gdk.RGBA(0.8, 0.8, 0.8, 1.0)
 CONF_BG = Gdk.RGBA(0.1, 0.1, 0.1, 1.0)
 
 class Terminal(Vte.Terminal):
-    def __init__(self, *args, **kwds):
+    def __init__(self, palette=None, *args, **kwds):
         super(Terminal, self).__init__(*args, **kwds)  
         self.set_cursor_blink_mode(Vte.CursorBlinkMode.ON)
         self.set_mouse_autohide(True)
         self.set_font(Pango.FontDescription(CONF_FONT))
-        self.set_colors(
-            foreground=CONF_FG,
-            background=CONF_BG,
-        )
+        if palette is None or len(palette) < 2:
+            self.set_colors(
+                foreground=CONF_FG,
+                background=CONF_BG,
+            )
+        else:
+            self.set_colors(
+                foreground=palette[0],
+                background=palette[1],
+            )
     
     def run_command(self, cmd):
         _cmd = str.encode(f"{cmd}\n")
@@ -59,8 +65,6 @@ class HeaderBar(Handy.HeaderBar):
 class MainWindow(Handy.ApplicationWindow):
     Handy.init()
     box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-    terminal = Terminal()
-    headerbar = HeaderBar(terminal)
 
     def __init__(
         self, 
@@ -69,11 +73,15 @@ class MainWindow(Handy.ApplicationWindow):
         env:list=[], 
         actions:list=[], 
         dark_theme:bool=False,
+        palette:list=[],
         *args, **kwds
     ):
         super().__init__()
         self.set_title(CONF_NAME)
         self.set_default_size(800, 450)
+
+        self.terminal = Terminal(palette)
+        self.headerbar = HeaderBar(self.terminal)
 
         if dark_theme:
             self.set_dark_theme()
@@ -121,9 +129,10 @@ class EasyTermLib:
         env:list=[], 
         actions:list=[], 
         dark_theme:bool=False, 
+        palette:list=[],
         *args, **kwds
     ):
-        self.window = MainWindow(cwd, command, env, actions, dark_theme)
+        self.window = MainWindow(cwd, command, env, actions, dark_theme, palette)
         self.window.show_all()
         self.window.connect("delete-event", Gtk.main_quit)
         Gtk.main()
@@ -191,6 +200,14 @@ class EasyTerm(Gtk.Application):
             "Set the dark theme",
             None
         )
+        self.add_main_option(
+            "palette",
+            ord("p"),
+            GLib.OptionFlags.NONE,
+            GLib.OptionArg.STRING,
+            "Set the palette (RGBA_back, RGBA_fore)",
+            None
+        )
 
     
     def do_command_line(self, command_line):
@@ -199,6 +216,7 @@ class EasyTerm(Gtk.Application):
         env = []
         actions = []
         dark_theme = False
+        palette = []
 
         options = command_line.get_options_dict()
         
@@ -206,15 +224,24 @@ class EasyTerm(Gtk.Application):
             cwd = options.lookup_value("cwd").get_string()
         if options.contains("command"):
             command = shlex.split(options.lookup_value("command").get_string())
-
         if options.contains("env"):
             env = options.lookup_value("env").get_string().split(" ")
         if options.contains("actions"):
             actions = options.lookup_value("actions").get_string().split(" ")
         if options.lookup_value("dark-theme").get_boolean():
             dark_theme = True
+        if options.contains("palette"):
+            palette = options.lookup_value("palette").get_string().split(" ")
+            if len(palette) < 2:
+                palette = []
+            else:
+                back = Gdk.RGBA()
+                back.parse(palette[0])
+                fore = Gdk.RGBA()
+                fore.parse(palette[1])
+                palette = [back, fore]
 
-        EasyTermLib(cwd, command, env, actions, dark_theme)
+        EasyTermLib(cwd, command, env, actions, dark_theme, palette)
         return 0
             
     def do_activate(self):
