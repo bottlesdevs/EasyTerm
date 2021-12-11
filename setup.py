5605ea2ec8afd7e70cb77820f7f361f89f2b39d3
mirkobrombin <send@mirko.pm>
2021-12-11 07:47:29 +0100

    0.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='An easy-to-bundle GTK terminal emulator',
     author='Mirko brombin',
     author_email='send@mirko.pm',
-    version='0.1.5',
+    version='0.1.6',
     packages=['easyterm'],
     scripts=['easyterm/easyterm.py'],
     url='https://github.com/bottlesdevs/EasyTerm',
